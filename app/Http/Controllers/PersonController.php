e6f4be88b3753aa471df5c6680830c7d0c4f579a
rashed-bishal <rashed.bishal@gmail.com>
2024-02-26 19:16:59 +0600

    Namespace in php

---
+++ app/Http/Controllers/PersonController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PersonController
+{
+    public function name()
+    {
+        return 'Rashed Bishal';
+    }
+
+    public function address()
+    {
+        return 'Dhaka, Bangladesh';
+    }
+};
