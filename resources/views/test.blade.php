1d492d97651f05d8f39bd7f905dfdb90a847b268
rashed.bishal <rashed.bishal@gmail.com>
2024-02-27 12:04:11 +0600

    Useful Blade Directories

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,3 +1,36 @@
 <h1>{{$str1}}</h1>
 
 <h2>{{$str2}}</h2>
+
+<!-- Useful Blade Directories 
+
+@php
+
+@endphp
+
+@isset()
+
+@endisset
+
+@empty()
+
+@endempty
+
+
+@switch()
+
+    @case()
+
+    @break
+
+    @case()
+
+    @break
+
+    @default
+
+@endswitch
+
+
+-->
+
