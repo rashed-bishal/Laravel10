108ce2a73cce38969ba347971b6685892901d309
rashed.bishal <rashed.bishal@gmail.com>
2024-02-29 16:51:40 +0600

    Data create and update using Mass Assignment

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -13,8 +13,19 @@ class WelcomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $post = Post::where('id',7)->delete();
+        // Post::create([
+        //     'title' => 'This is my custom title',
+        //     'description' => 'This is my custom description',
+        //     'status' => false,
+        //     'published_date' => date('Y-m-d'),
+        //     'user_id' => 2204,
+        //     'category_id' => 6,
+        // ]);
 
-        dd('Data successfully erased');
+        Post::where('id', 9)->update([
+            'description' => 'Custom Description has been updated!',
+        ]);
+
+        dd('Post Updated');
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,4 +9,8 @@ class Post extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'title', 'description', 'status', 'published_date', 'user_id', 'category_id',
+    ];
+
 }
