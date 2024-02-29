8674f54b146315f181243fd10f3852cf588f621e
rashed.bishal <rashed.bishal@gmail.com>
2024-02-29 17:32:12 +0600

    Introduced Softdeletes and applied

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -13,19 +13,10 @@ class WelcomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-        // Post::create([
-        //     'title' => 'This is my custom title',
-        //     'description' => 'This is my custom description',
-        //     'status' => false,
-        //     'published_date' => date('Y-m-d'),
-        //     'user_id' => 2204,
-        //     'category_id' => 6,
-        // ]);
+        return Post::all();
+        //Post::where('id',1)->delete();
+        //dd('Post Deleted');
 
-        Post::where('id', 9)->update([
-            'description' => 'Custom Description has been updated!',
-        ]);
 
-        dd('Post Updated');
     }
 }
