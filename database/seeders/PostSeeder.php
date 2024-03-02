4771fe80287f6f945c4f7241815f4505d4b63cbe
rashed.bishal <rashed.bishal@gmail.com>
2024-03-02 12:36:41 +0600

    Factories

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use App\Models\Post;
 
 
 class PostSeeder extends Seeder
@@ -15,67 +16,7 @@ class PostSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('posts')->create([
-            'title' => '1st Post',
-            'description' => 'This is inside First Post',
-            'status' => TRUE,
-            'published_date' => date('Y-m-d'),
-            'user_id' => 2063,
-        ]);
-
-        DB::table('posts')->insert([
-            'title' => '2nd Post',
-            'description' => 'This is inside Second Post',
-            'status' => TRUE,
-            'published_date' => date('Y-m-d'),
-            'user_id' => 1040,
-        ]);
-
-        DB::table('posts')->insert([
-            'title' => '3rd Post',
-            'description' => 'This is inside Third Post',
-            'status' => TRUE,
-            'published_date' => date('Y-m-d'),
-            'user_id' => 2450,
-        ]);
-
-        DB::table('posts')->insert([
-            'title' => '4th Post',
-            'description' => 'This is inside Fourth Post',
-            'status' => TRUE,
-            'published_date' => date('Y-m-d'),
-            'user_id' => 6410,
-        ]);
-
-        DB::table('posts')->insert([
-            'title' => '5th Post',
-            'description' => 'This is inside Fifth Post',
-            'status' => TRUE,
-            'published_date' => date('Y-m-d'),
-            'user_id' => 8070,
-        ]);
-
-        DB::table('posts')->insert([
-            'title' => '6th Post',
-            'description' => 'This is inside Sixth Post',
-            'status' => TRUE,
-            'published_date' => date('Y-m-d'),
-            'user_id' => 7650,
-        ]);
-        
-
-        DB::table('categories')->insert([
-            'name' => 'News',
-        ]);
-
-        DB::table('categories')->insert([
-            'name' => 'Tech',
-        ]);
-
-        DB::table('categories')->insert([
-            'name' => 'Education',
-        ]);
-
+        Post::factory(250)->create();
 
     }
 }
