047c8a3916af69b5322e76c6016bdb055c9a49f4
rashed.bishal <rashed.bishal@gmail.com>
2024-03-03 10:38:53 +0600

    Many to many relationships, attaching & detaching tables

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\Category;
+use App\Models\Tag;
+
 
 class AboutController extends Controller
 {
@@ -13,7 +15,21 @@ class AboutController extends Controller
      */
     public function __invoke(Request $request)
     {
-       $posts = Category::find(3)->posts;
-       return view('test', compact('posts'));
+        // for($i=101; $i<406; $i++)
+        // {
+        //     $post = Post::find($i);
+
+        //     $post->tags()->attach(rand(1,4));
+        // }
+
+        // dd('suxsex');
+
+        // $posts = Tag::find(4)->posts;
+
+        // return $posts;
+
+        $tags= Post::find(140)->tags;
+
+        return $tags;
     }
 }
