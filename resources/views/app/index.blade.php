361f0cdca78f4f1950e57dc7b252b0b75c1fd1b9
rashed-bishal <rashed.bishal@gmail.com>
2024-03-07 00:05:29 +0600

    Creating front end for CRUD

---
+++ resources/views/app/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.master')
+
+@section('content')
+
+<div class="card">
+  <div class="card-body">
+  <table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Image</th>
+      <th scope="col">Title</th>
+      <th scope="col">Description</th>
+      <th scope="col">Category</th>
+      <th scope="col">Published Date</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+  </div>
+</div>
+
+@endsection
