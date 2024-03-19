72c3e98062ddfff8a27d7a9b9eb9d15704d0978f
rashed-bishal <rashed.bishal@gmail.com>
2024-03-20 04:34:26 +0600

    Home Enviornment updated

--- app/Http/Controllers/SampleController.php
+++ app/Http/Controllers/SampleController.php
@@ -4,35 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Services\PaymentService;
-<<<<<<< HEAD
-use App\Services\CategoryService;
-use App\Models\Post;
-use App\Models\Category;
-=======
 use App\Facades\CustomFacade;
->>>>>>> 96cca8b26a102880f1112a37cbc2a27c822f62bc
 
 class SampleController extends Controller
 {
 
-<<<<<<< HEAD
-    public function __construct(public Post $posts, public Category $categories)
-    {
-
-=======
     private $paymentService;
 
     public function __construct(PaymentService $paymentService)
     {
         $this->paymentService = $paymentService;
->>>>>>> 96cca8b26a102880f1112a37cbc2a27c822f62bc
     }
 
     public function index()
     {
-<<<<<<< HEAD
-        return $this->posts->all();
-=======
         //return $this->paymentService->paymentMethod();
 
         dd(app());
@@ -41,6 +26,5 @@ class SampleController extends Controller
     public function custom()
     {
        echo CustomFacade::messageMethod();
->>>>>>> 96cca8b26a102880f1112a37cbc2a27c822f62bc
     }
 }
