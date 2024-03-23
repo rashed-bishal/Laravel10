2b6656f91a072ed459b74e70fc399593b4dd5f03
Rashed Bishal <rashed.bishal@gmail.com>
2024-03-23 16:02:15 +0600

    Recreating Logout feature

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -20,7 +20,9 @@
             <li><a class="dropdown-item" href="#">Action</a></li>
             <li><a class="dropdown-item" href="#">Another action</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><form action="{{route('logout')}}" method="POST">
+              @csrf <button class="dropdown-item" type="submit">Logout</button>
+            </form></li>
           </ul>
         </li>
         <li class="nav-item">
